180d859a6568e5d0923ced787c2841def8840367
jaaaar <yuwen2424@163.com>
2018-03-03 15:50:28 +0800

    commit cookie

--- src/main/java/com/mmall/controller/portal/UserController.java
+++ src/main/java/com/mmall/controller/portal/UserController.java
@@ -33,7 +33,7 @@ public class UserController {
     @RequestMapping(value = "login.do", method = RequestMethod.POST)
     @ResponseBody
     public ServerResponse<User> login(String username, String password,
-                                      HttpSession session, HttpServletResponse servletResponse,) {
+                                      HttpSession session, HttpServletResponse servletResponse) {
         ServerResponse<User> response =  iUserService.login(username, password);
         if (response.isSuccess()) {
 //            session.setAttribute(Const.CURRENT_USER, response.getData());
